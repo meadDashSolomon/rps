c4eb46d1b4e678fa7ae0f7c96aa6d5dfd35a2945
Mead Solomon <mead93@gmail.com>
2022-11-22 09:47:05 -0600

    Should've made more commits! near completion :o

--- main.js
+++ main.js
@@ -1,20 +1,62 @@
 const choices = ["rock", "paper", "scissors"];
+const winners = [];
 
 function game() {
-    playRound()
+    for (let i = 0; i < 5; i++) {
+        playRound();
+     }
+     logWins();
 }
 
 function playRound() {
-const playerSelection = playerChoice();
-const computerSelection = computerChoice();
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+    const winner = checkWinner(playerSelection, computerSelection);
+    winners.push(winner);
 }
 
 function playerChoice() {
     let input = prompt("Type Rock, Paper, or Scissors");
+    while (input == null) {
+        input = prompt("Type Rock, Paper, or Scissors");
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input);
+    while (check == false) {
+        input = prompt("Type rps capitalkjoijlj");
+        while (input == null) {
+            input = prompt("Type Rock, Paper, or Scissors");
+        }
+    input = input.toLowerCase();
+    check = validateInput(input);
+    }
+    return input;
 }
 
 function computerChoice() {
     return choices[Math.floor(Math.random() * choices.length)]
+}
+
+function validateInput(choice) {
+    return choices.includes(choice);
+}
+
+function checkWinner(choiceP, choiceC) {
+    if (choiceP === choiceC) {
+        return "Tie";
+    } else if (
+        (choiceP === "rock" && choiceC == "scissors") || 
+        (choiceP === "paper" && choiceC == "rock") || 
+        (choiceP === "scissors" && choiceC == "paper")
+    ) {
+        return 'Player';
+    } else {
+        return 'Computer';
+    }
+}
+
+function logWins(){
+    console.log(winners);
 }
 
 game();
